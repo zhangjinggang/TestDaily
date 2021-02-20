8649e381653b9cdf688c9fabcf513f4043a1d9ff
张京港 <zjg01@choicesoft.com.cn>
2021-02-20 14:35:22 +0800

    json优美输出

---
+++ TestDaily-static/src/main/java/tool/PrintJSONStr.java
@@ -0,0 +1,15 @@
+package tool;
+
+/**
+ * @author zjg
+ * @description:
+ * @date 2021/1/29 15:43
+ */
+public class PrintJSONStr {
+
+    public static void main(String[] args) {
+//        UpdatePickUpMarkSettingRequest request = new UpdatePickUpMarkSettingRequest();
+//        System.out.println(JSONObject.toJSONString(request, SerializerFeature.PrettyFormat,SerializerFeature.WriteMapNullValue));
+
+    }
+}
