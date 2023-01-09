b2ba9d32eee9f0dcfc124e30db6118b77d8bbe4a
zjg <1017900650@qq.com>
2023-01-10 00:59:48 +0800

    提交测试

--- TestDaily-dao/src/main/java/com/zjg/dao/config/DynamicDataSource.java
+++ TestDaily-dao/src/main/java/com/zjg/dao/config/DynamicDataSource.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
     private Logger log = LoggerFactory.getLogger(DynamicDataSource.class);
 
     @Override
